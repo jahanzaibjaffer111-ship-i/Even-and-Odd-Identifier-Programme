7f75f9544fc0ba48341f5d824645ee7a47b5835e
Jahanzaib Jaffer Ali <jahanzaib.jaffer111@gmail.com>
2026-01-12 12:18:21 +0500

    Add files via upload

---
+++ evenandoddprogramme.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class evenandoddprogramme
+{
+    public static void main(String[] args)
+    {
+        try (Scanner read = new Scanner(System.in)) {
+            int a;
+            while(1>0)
+            {
+                System.out.println("HELLO");
+                System.out.println("WELCOME TO EVEN AND ODD CHECK PROGRAMME");
+                System.out.println("TYPE VALUE");
+                a=read.nextInt();
+                if (a % 2 == 0)
+                {
+                    System.out.println("EVEN");
+                }
+                if (a % 2 != 0)
+                {
+                    System.out.println("ODD");
+                }
+                System.out.println("THANKS");
+                System.out.println("BYE");
+                System.out.println(".");
+
+            }
+        }
+
+
+    }
+}
